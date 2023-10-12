b0040845c3cf3e5879ca24b4ac7a799600a1b63d
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-13 05:25:46 +0900

    사용하지 않는 폰트 제거

---
+++ app/src/main/java/com/kenshi/expandablelist/compose/ui/theme/Font.kt
@@ -0,0 +1,14 @@
+package com.kenshi.expandablelist.compose.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.kenshi.expandablelist.R
+
+val pretendard = FontFamily(
+    Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.pretendard_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.pretendard_semi_bold, FontWeight.SemiBold, FontStyle.Normal),
+)
