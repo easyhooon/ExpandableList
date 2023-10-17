662d2d29dc3f56fddb06bd7c33712ad39d08ffc8
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-17 14:39:19 +0900

    사용하지 않는 변수 제거

--- app/src/main/java/com/kenshi/expandablelist/view/NoticeViewHolder.kt
+++ app/src/main/java/com/kenshi/expandablelist/view/NoticeViewHolder.kt
@@ -6,10 +6,7 @@ import com.kenshi.expandablelist.NoticeItem
 import com.kenshi.expandablelist.databinding.ItemNoticeBinding
 
 class NoticeViewHolder(val binding: ItemNoticeBinding) : RecyclerView.ViewHolder(binding.root) {
-    private var currentItem: NoticeItem? = null
-
     fun bind(notice: NoticeItem) {
-        currentItem = notice
         binding.apply {
             tvNoticeTitle.text = notice.title
             tvNoticeDate.text = notice.date
